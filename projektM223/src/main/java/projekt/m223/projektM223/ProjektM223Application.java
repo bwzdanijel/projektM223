102ffbe2ac8207d554ae014931e0b84d975d3335
bwzdanijel <danijel.ilic@bwz-rappi.ch>
2024-05-15 14:17:05 +0200

    Initial commit

---
+++ projektM223/src/main/java/projekt/m223/projektM223/ProjektM223Application.java
@@ -0,0 +1,13 @@
+package projekt.m223.projektM223;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektM223Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektM223Application.class, args);
+	}
+
+}
---
+++ projektM223/src/test/java/projekt/m223/projektM223/ProjektM223ApplicationTests.java
@@ -0,0 +1,13 @@
+package projekt.m223.projektM223;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektM223ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
