0833d081276b312495e3321f56397993b8dd5e42
Mademidda Saseenthiran <mademidda.saseenthiran@hotmail.com>
2024-06-21 11:08:32 +0200

    compare Reservation by date

--- projektM223/src/main/java/projekt/m223/projektM223/service/ReservationService.java
+++ projektM223/src/main/java/projekt/m223/projektM223/service/ReservationService.java
@@ -1,10 +1,14 @@
 package projekt.m223.projektM223.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import projekt.m223.projektM223.model.ReservationModel;
 import org.springframework.stereotype.Service;
+import projekt.m223.projektM223.model.ReservationModel;
 import projekt.m223.projektM223.repository.ReservationRepository;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
 
 @Service
 public class ReservationService {
@@ -20,26 +24,39 @@ public class ReservationService {
         return reservationRepository.findByPrivateCode(privateCode);
     }
 
-    public ReservationModel saveReservation(ReservationModel reservation) {
+    public ReservationModel saveReservation(ReservationModel reservation) throws Exception {
+        validateReservation(reservation);
         return reservationRepository.save(reservation);
     }
 
-    public ReservationModel updateReservationByPrivateCode(String privateCode, ReservationModel reservationDetails) {
-        ReservationModel reservation = reservationRepository.findByPrivateCode(privateCode);
-        if (reservation != null) {
-            reservation.setDate(reservationDetails.getDate());
-            reservation.setFrom(reservationDetails.getFrom());
-            reservation.setTo(reservationDetails.getTo());
-            reservation.setRoom(reservationDetails.getRoom());
-            reservation.setComment(reservationDetails.getComment());
-            reservation.setMemberList(reservationDetails.getMemberList());
-            return reservationRepository.save(reservation);
+    public ReservationModel updateReservationByPrivateCode(String privateCode, ReservationModel reservation) throws Exception {
+        ReservationModel existingReservation = reservationRepository.findByPrivateCode(privateCode);
+        if (existingReservation == null) {
+            throw new Exception("Reservation not found.");
         }
-        return null;
+        validateReservation(reservation);
+        existingReservation.setDate(reservation.getDate());
+        existingReservation.setFrom(reservation.getFrom());
+        existingReservation.setTo(reservation.getTo());
+        existingReservation.setRoom(reservation.getRoom());
+        existingReservation.setComment(reservation.getComment());
+        existingReservation.setMemberList(reservation.getMemberList());
+        return reservationRepository.save(existingReservation);
     }
 
     public void deleteReservation(ReservationModel reservation) {
         reservationRepository.delete(reservation);
     }
-}
 
+    private void validateReservation(ReservationModel reservation) throws Exception {
+        LocalDate today = LocalDate.now();
+
+        // Convert from java.util.Date to  LocalDate
+        Instant instant = reservation.getDate().toInstant();
+        LocalDate reservationDate = instant.atZone(ZoneId.systemDefault()).toLocalDate();
+
+        if (reservationDate.isBefore(today)) {
+            throw new Exception("Reservation date cannot be in the past.");
+        }
+    }
+}
